faa56cff2c6054057bd2c7d5eea8d97534bfc1c1
Artem Kalinovsky <artem.kalinovsky@gmail.com>
2023-05-01 11:09:02 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# XcodeProjectPlugins
+
+A description of this package.
---
+++ Sources/XcodeProjectPlugins/XcodeProjectPlugins.swift
@@ -0,0 +1,6 @@
+public struct XcodeProjectPlugins {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
