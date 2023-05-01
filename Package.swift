a1de244704cdac658453a65de4bc8cf4f5da3bfd
Artem Kalinovsky <artem.kalinovsky@gmail.com>
2023-05-01 12:05:58 +0200

    GenerateLocalisationEnumPlugin

--- Package.swift
+++ Package.swift
@@ -5,24 +5,31 @@ import PackageDescription
 
 let package = Package(
     name: "XcodeProjectPlugins",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "XcodeProjectPlugins",
-            targets: ["XcodeProjectPlugins"]),
+    platforms: [
+        .iOS(.v13),
+        .watchOS(.v6),
+        .macOS(.v10_15),
+        .tvOS(.v13),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .plugin(
+            name: "GenerateLocalisationEnumPlugin",
+            targets: ["GenerateLocalisationEnumPlugin"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "XcodeProjectPlugins",
-            dependencies: []),
-        .testTarget(
-            name: "XcodeProjectPluginsTests",
-            dependencies: ["XcodeProjectPlugins"]),
+        .plugin(
+            name: "GenerateLocalisationEnumPlugin",
+            capability: .buildTool()
+        )
+//        .plugin(
+//            name: "SwiftLintFix",
+//            capability: .command(
+//                intent: .sourceCodeFormatting(),
+//                permissions: [.writeToPackageDirectory(reason: "Fixes fixable lint issues")]
+//            ),
+//            dependencies: ["SwiftLintBinary"]
+//        ),
     ]
 )
+
